cb116c3e3a2750c802b2ddfa854265b17e0218cf
Andreas Hässler <andreas.haessler@outlook.at>
2017-10-28 18:31:06 +0200

    Add opposites to directions

--- src/directions.js
+++ src/directions.js
@@ -1,8 +1,12 @@
-const Directions = {
+export const directions = {
     up: 0,
     right: Math.PI * 0.5,
     down: Math.PI,
     left: Math.PI * 1.5
 }
 
-export default Directions
+export const opposites = {}
+opposites[directions.up] = directions.down
+opposites[directions.down] = directions.up
+opposites[directions.left] = directions.right
+opposites[directions.right] = directions.left
