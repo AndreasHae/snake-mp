654417ad73adb92bdc0fae5aa23488ee4c888b02
Andreas Hässler <andreas.haessler@outlook.at>
2017-10-29 16:07:02 +0100

    Add babel and webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,19 @@ module.exports = {
         filename: "game.js",
         path: path.resolve(__dirname, "dist")
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    },
     devtool: "inline-source-map",
 }
